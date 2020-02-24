422855fdaf2c94af67de97f34699ab7357cecf1c
pcerimeli <mecacerimeli@gmail.com>
2020-02-24 14:43:47 -0300

    desafio 01

--- calculoImc.js
+++ calculoImc.js
@@ -19,7 +19,7 @@ const imc  = peso/(altura*altura);
 //SE o IMC maior ou igual a 30: Carlos você está acima do peso;
 //SE o IMC menor que 29.9: Carlos você não está acima do peso;
 
-if (imc >= 30){
+ if (imc >= 30){
                console.log("Carlos você está acima do peso")  
 }else{
                 console.log("Carlos você não está acima do peso")
